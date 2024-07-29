7be81112421c316ae968717511711e508dc2a669
Diana Birsan <diana.birsan@3pillarglobal.com>
2024-07-29 10:42:01 +0300

    feat: add products and orders tables

--- eshop/main.go
+++ eshop/main.go
@@ -12,7 +12,22 @@ import (
 )
 
 type userType struct {
-	firstname, lastname, address, age string
+	firstname, lastname, username, password, email, city, street, number, additional_info string
+	zipcode, age                                                                          int64
+}
+
+type productType struct {
+	id, name, description, currency string
+	count                           int64
+	price                           float64
+}
+
+type ordersType struct {
+	id, username                string
+	items                       string
+	status, delivery            string
+	delivery_price, total_price float64
+	currency                    string
 }
 
 func main() {
@@ -20,7 +35,7 @@ func main() {
 		Scheme:   "postgres",
 		Host:     net.JoinHostPort("localhost", strconv.Itoa(5432)),
 		Path:     "eshop",
-		User:     url.UserPassword("user", "password"),
+		User:     url.UserPassword("admin", "admin"),
 		RawQuery: "sslmode=disable",
 	}
 
@@ -36,11 +51,43 @@ func main() {
 	}
 	for rows.Next() {
 		var user userType
-		err := rows.Scan(&user.firstname, &user.lastname, &user.address, &user.age)
+		err := rows.Scan(&user.firstname, &user.lastname, &user.username, &user.password, &user.email, &user.city, &user.zipcode, &user.street, &user.number, &user.additional_info, &user.age)
 		if err != nil {
 			log.Fatal(err)
 		}
 		fmt.Println(user)
+	}
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	rows, err = db.Query("SELECT * FROM products")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for rows.Next() {
+		var prod productType
+		err := rows.Scan(&prod.id, &prod.name, &prod.description, &prod.currency, &prod.count, &prod.price)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(prod)
+	}
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	rows, err = db.Query("SELECT * FROM orders")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for rows.Next() {
+		var order ordersType
+		err := rows.Scan(&order.id, &order.username, &order.items, &order.status, &order.delivery, &order.delivery_price, &order.total_price, &order.currency)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(order)
 	}
 	if err := rows.Err(); err != nil {
 		log.Fatal(err)
---
+++ eshop/queries.sql
@@ -0,0 +1,119 @@
+/*
+USERS
+
+	First name
+	Last name
+    Username
+    Password
+    Email
+	City
+    ZIP codeStreet 
+    Number
+    Addition info
+    Age
+
+*/
+
+CREATE TABLE users (
+  firstname VARCHAR(255),
+  lastname VARCHAR(255),
+  username VARCHAR(255) PRIMARY KEY,
+  password VARCHAR(255),
+  email VARCHAR(255),
+  city VARCHAR(255),
+  zipcode INT,
+  street VARCHAR(255),
+  number VARCHAR(255),
+  additional_info VARCHAR(255),
+  age INT
+);
+
+
+INSERT INTO users (firstname,
+  lastname,
+  username,
+  password,
+  email,
+  city,
+  zipcode,
+  street,
+  number,
+  additional_info,
+  age)
+VALUES 
+	('Jane', 'Doe', 'janedoe1','password1', 'jane@yahoo.com', 'New York', 223423, 'Street1', '24A','',27),
+  ('John', 'Doe', 'johndoe', 'password2', 'john@yahoo.com', 'New York', 223423, 'Street1', '24A','',27);
+
+
+/*
+PRODUCTS
+
+	ID 
+	Name
+	Count 
+	Description
+	Price
+	Currency
+
+*/
+
+
+CREATE TABLE products (
+  id VARCHAR(255) PRIMARY KEY,
+  name VARCHAR(255),
+  description VARCHAR(255),
+  currency VARCHAR(255),
+  count INT,
+  price FLOAT
+);
+
+
+INSERT INTO products (id,
+  name,
+  description,
+  currency,
+  count,
+  price)
+VALUES 
+	('P432','TV1', 'desc1', 'euro', 4, 78.9),
+  ('P433','TV2', 'desc2', 'euro', 7, 100.56);
+
+
+/*
+ORDERS
+
+	ID
+	Username
+	Items -> [product_id:count, …] (json) { “product_id”: 5 }
+	Status
+	Delivery
+	Delivery price
+	Total price
+	Currency
+
+
+*/
+
+CREATE TABLE orders (
+  id VARCHAR(255) PRIMARY KEY,
+  username VARCHAR(255),
+  items JSONB,
+  status VARCHAR(255),
+  delivery VARCHAR(255),
+  delivery_price FLOAT,
+  total_price FLOAT,
+  currency VARCHAR(255)
+);
+
+
+INSERT INTO orders (id,
+  username,
+  items,
+  status,
+  delivery,
+  delivery_price,
+  total_price,
+  currency)              
+VALUES 
+	('O1','janedoe1', '{"P432": 1}', 'In Progress', 'Easybox', 10, 700, 'euro'),
+  ('O2','johndoe', '{"P432": 1,"P433": 1}', 'Finished', 'DPD', 10, 999.9, 'usd');
