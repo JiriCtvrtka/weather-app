56577c9d2e3bc097a376ba32f8b9e3c50f83c219
Diana Birsan <diana.birsan@3pillarglobal.com>
2024-10-07 11:20:32 +0300

    fix: remove username from create user

--- eshop/users/create.go
+++ eshop/users/create.go
@@ -15,10 +15,9 @@ func Create(item models.UserType) (string, error) {
 	}
 	defer db.Close()
 
-	var username string
-	err = db.QueryRow(
-		"INSERT INTO users (firstname, lastname, username, password, email, city, street, number, additional_info, zipcode, age) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) returning username",
-		item.Firstname, item.Lastname, item.Username, item.Password, item.Email, item.City, item.Street, item.Number, item.AdditionalInfo, item.Zipcode, item.Age).Scan(&username)
+	_, err = db.Exec(
+		"INSERT INTO users (firstname, lastname, username, password, email, city, street, number, additional_info, zipcode, age) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)",
+		item.Firstname, item.Lastname, item.Username, item.Password, item.Email, item.City, item.Street, item.Number, item.AdditionalInfo, item.Zipcode, item.Age)
 	if err != nil {
 		fmt.Println(err)
 		return "", err
@@ -28,5 +27,5 @@ func Create(item models.UserType) (string, error) {
 		return "", err
 	}
 
-	return username, nil
+	return item.Username, nil
 }
